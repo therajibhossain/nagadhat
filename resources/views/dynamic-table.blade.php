2ed7897c534e40dc866aea7a7e7067f397cb5258
Rajib Hossain <rajibhossain.php@gmail.com>
2022-03-05 04:01:04 +0600

    frontend menu update

--- resources/views/dynamic-table.blade.php
+++ resources/views/dynamic-table.blade.php
@@ -1,73 +1,63 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.frontend')
 
-        <title>Dynamic Table</title>    
+@section('content')
+    <style>
+        table {
+            width: 100%;
+        }
+        .table {
+            width: 50%;
+            margin-left: 23%
+        }
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+    </style>
 
-            table {
-                width: 100%;
-            }
-            .table {
-                width: 50%;
-                margin-left: 23%
-            }
+    <div class="row">
+        <h3 class="text-2xl font-medium text-gray-700" style="margin-bottom: 20px; margin-top: -20px; text-align: center">Dynamic Table</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Company's Name</th>
+                    <th>Branch's Name</th>
+                    <th>Staff's Name</th>
+                </tr>
+            </thead>
 
-            td, th {
-                border: 1px solid #dddddd;
-                text-align: left;
-                padding: 8px;
-            }
-        </style>
-    </head>
-    <body class="antialiased">
-        <div class="row">
-            <table class="table">
-                <thead>
+            <tbody>
+                @foreach($tableData as $company) 
                     <tr>
-                        <th>Company's Name</th>
-                        <th>Branch's Name</th>
-                        <th>Staff's Name</th>
+                        <th rowspan="{{ count($company["branches"]) }}">{{ $company["title"] }}</th>
+                        <th>{{ $company["branches"][0]["title"] }}</th>
+                        <td>
+                            <table class="staff">
+                                @foreach($company["branches"][0]["staffs"] as $staff)
+                                    <tr><td>{{ $staff }}</td></tr>
+                                @endforeach
+                            </table>
+                        </td>
                     </tr>
-                </thead>
 
-                <tbody>
-                    @foreach($tableData as $company) 
-                        <tr>
-                            <th rowspan="{{ count($company["branches"]) }}">{{ $company["title"] }}</th>
-                            <th>{{ $company["branches"][0]["title"] }}</th>
-                            <td>
-                                <table class="staff">
-                                    @foreach($company["branches"][0]["staffs"] as $staff)
-                                        <tr><td>{{ $staff }}</td></tr>
-                                    @endforeach
-                                </table>
-                            </td>
-                        </tr>
-
-                        @foreach($company["branches"] as $branch)
-                            @if($loop->index > 0)
-                                <tr>
-                                    <th>{{ $branch["title"] }}</th>
-                                    <td>
-                                        <table class="staff">
-                                            @foreach($branch["staffs"] as $staff)
-                                                <tr><td>{{ $staff }}</td></tr>
-                                            @endforeach
-                                        </table>
-                                    </td>
-                                </tr>
-                            @endif
-                        @endforeach
+                    @foreach($company["branches"] as $branch)
+                        @if($loop->index > 0)
+                            <tr>
+                                <th>{{ $branch["title"] }}</th>
+                                <td>
+                                    <table class="staff">
+                                        @foreach($branch["staffs"] as $staff)
+                                            <tr><td>{{ $staff }}</td></tr>
+                                        @endforeach
+                                    </table>
+                                </td>
+                            </tr>
+                        @endif
                     @endforeach
-                </tbody>
-            </table>
-        </div>
-    </body>
-</html>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add to cart</title>
+    <title>Laravel Full stack Developer Assessment Exam</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        .hover\:underline:hover {
+            text-decoration: none;
+            color: green;
+        }
+    </style>
 </head>
 <body>
     <div  class="bg-white">
@@ -24,6 +30,7 @@
                     <div class="flex flex-col sm:flex-row">
                         <a class="mt-3 hover:underline sm:mx-3 sm:mt-0" href="/">Home</a>
                         <a class="mt-3 hover:underline sm:mx-3 sm:mt-0" href="{{ route('products.list')}}">Shop</a>
+                        <a class="mt-3 hover:underline sm:mx-3 sm:mt-0" href="{{ url('dynamic-table')}}">Dynamic Table</a>
                         <a href="{{ route('cart.list') }}" class="flex items-center">
                             <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
